de4dde5b181e5e5889d833aeb01c4872dfc7475d
galya <galya@DESKTOP-3LFE8U5>
2023-09-15 01:01:16 +0700

    jhyg

--- Program.cs
+++ Program.cs
@@ -116,32 +116,36 @@ namespace begin_доп_задания
 
         static void qwe1()
         {
+
             int a;
             Console.WriteLine("a=");
             string t = Console.ReadLine();
             a = int.Parse(t);
             string r = "рублей";
+            
 
-
-
-            if ((a / 10 % 10 == 2 || a / 10 % 10 == 3 || a / 10 % 10 == 4 || a / 10 % 10 == 5 || a / 10 % 10 == 6 || a / 10 % 10 == 7 || a / 10 % 10 == 8 || a / 10 % 10 == 9) & a % 10 == 1)
-            {
-                Console.WriteLine(a + "рубль");
-            }
-            else if ((a / 10 % 10 == 2 || a / 10 % 10 == 3 || a / 10 % 10 == 4 || a / 10 % 10 == 5 || a / 10 % 10 == 6 || a / 10 % 10 == 7 || a / 10 % 10 == 8 || a / 10 % 10 == 9) && (a % 10 == 2 || a % 10 == 3 || a % 10 == 4))
-            {
-                Console.WriteLine(a + "рубля");
-            }
            
+            
+            if ((a / 10 % 10 > 1 & a / 10 % 10 < 10) & a % 10 == 1)
+                {
+                    Console.WriteLine(a + "рубль");
+                }
+                else if ((a / 10 % 10 > 1 & a / 10 % 10 < 10) && (a % 10 == 2 || a % 10 == 3 || a % 10 == 4))
+                {
+                    Console.WriteLine(a + "рубля");
+                }
 
-            else if (a / 10 == 1 & (a % 10 == 1 || a % 10 == 2 || a % 10 == 3 || a % 10 == 4 || a % 10 == 5 || a % 10 == 6 || a % 10 == 7 || a % 10 == 8 || a % 10 == 9))
-            {
-                Console.WriteLine(a + r);
-            }
-            else
-            {
-                Console.WriteLine(a + r);
-            }
+                else if ((a % 10 == 1 || a % 10 == 2 || a % 10 == 3 || a % 10 == 4 || a % 10 == 5 || a % 10 == 6 || a % 10 == 7 || a % 10 == 8 || a % 10 == 9))
+                {
+                    Console.WriteLine(a + r);
+                }
+                else
+                {
+                    Console.WriteLine(a + r);
+                }
+            
+
+
             //double s;
             //Console.WriteLine("введите сумму");
             //string w = Console.ReadLine();
